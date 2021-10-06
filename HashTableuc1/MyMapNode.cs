cc5041ebc4a02a5a1a6ab419e48cd21fd7440962
varnikak00 <varnikak7070@gmail.com>
2021-10-06 11:17:15 +0530

    hashtableuc1

---
+++ HashTableuc1/MyMapNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableuc1
+{
+    class MyMapNode<K,V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)//constructor
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+
+        }
+        /// <summary>
+        /// method to add 
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <param name="value"></param>
+        public void Add(K Key, V value)
+        {
+            int position = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = Key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        /// <summary>
+        /// method to find array position
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public struct KeyValue<k, v>
+        {
+
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+    }
+}
+
---
+++ HashTableuc1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HashTableuc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("The value of 5th index is: {0} ", hash5);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("The value of 2nd index is: {0} ", hash2);
+
+        }
+    }
+}
